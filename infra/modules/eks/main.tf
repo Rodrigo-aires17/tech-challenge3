9437b9e84f28aa2d809675fe7278a341b3fb6209
Rodrigo-aires17 <72283194+Rodrigo-aires17@users.noreply.github.com>
2026-09-15 18:19:10 -0300

    fix: corrigir nome do grupo de nós EKS para incluir prefixo correto

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -18,7 +18,7 @@ resource "aws_eks_cluster" "this" {
 
 resource "aws_eks_node_group" "default" {
   cluster_name    = aws_eks_cluster.this.name
-  node_group_name = "${var.name_prefix}-default"
+  node_group_name = "${var.name_prefix}-eks-node-group"
   node_role_arn   = var.node_role_arn
   subnet_ids      = var.private_subnet_ids
   instance_types  = var.node_instance_types
